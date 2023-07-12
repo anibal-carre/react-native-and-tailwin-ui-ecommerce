fc43b5cd845d4840c3d4107e379f863d853c9f94
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2023-07-12 13:29:39 -0300

    final

---
+++ components/ProductsLists.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { FlatList } from "react-native";
+import { products } from "../products";
+import { Text } from "react-native";
+import ProductCard from "./ProductCard";
+
+export default function ProductsList() {
+  return (
+    <FlatList
+      data={products}
+      keyExtractor={(product) => product.id}
+      renderItem={({ item }) => <ProductCard {...item} />}
+      contentContainerStyle={{ paddingHorizontal: 15 }}
+    />
+  );
+}
